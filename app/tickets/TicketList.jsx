f5c1b2468e477708e61c994823b738655553605d
Soichiro Wada <soichirowada@ipmulticast.net>
2023-11-26 01:21:23 +0900

    ticketlist committed

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 async function getTickets() {
-    const res = await fetch('http://192.168.1.20:4000/tickets')
-
+    const res = await fetch('http://192.168.1.20:4000/tickets', {
+        next: {
+            revalidate: 10
+        }
+    })
     return res.json()
 }
 
@@ -13,11 +17,13 @@ export default async function TicketList() {
     <>
         {tickets.map((ticket) => (
             <div key={ticket.id} className="card my-5">
-                <h3>{ticket.title}</h3>
-                <p>{ticket.body.slice(0,200)}...</p>
-                <div className={`pill ${ticket.priority}`}>
-                    {ticket.priority} priority
-                </div>
+                <Link href={`/tickets/${ticket.id}`}>
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0,200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </Link>
             </div>
         
         ))}
---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+
+async function getTicket(id) {
+    const res = await fetch(`http://192.168.1.20:4000/tickets/${id}`, {
+        next: {
+            revalidate: 10
+        }
+    })
+    return res.json()
+}
+
+export default async function TicketDetails({ params }) {
+    const ticket = await getTicket(params.id)
+
+    return (
+        <main>
+            <nav>
+                <h2>Ticket Details</h2>
+            </nav>
+            <div className="card">
+                <h3>{ticket.title}</h3>
+                <small>Created by {ticket.user_email}</small>
+                <p>{ticket.body}</p>
+                <div className={`pill ${ticket.priority}`}>
+                    {ticket.priority} priority
+                </div>
+                <Link href={`/tickets`}>
+                    <button>{"Return to Tickets"}</button>
+                </Link>
+            </div>
+        </main>
+    )
+}
